0a52b9f458743e75af6870dfcf554a3ad0a04bcc
lemon-cake <kaique-lemos@hotmail.com>
2024-10-24 13:27:59 +0200

    settup debut

---
+++ main.js
@@ -0,0 +1,9 @@
+/* But :
+-Afficher la structure de Yannick en js.
+
+-La totalité du site est dans la div root.
+*/
+
+//La seulle div dans le fichier html:
+
+const root = document.getElementById()
