ebde0119562d40f96e0f9b099b1d5cff14ca1702
lemon-cake <kaique-lemos@hotmail.com>
2024-10-28 10:35:15 +0100

    sylv js check

---
+++ sylvain.js
@@ -0,0 +1,193 @@
+
+const root = document.getElementById('root');
+const zoneQuizz =  document.createElement('session');
+const questionDiv = document.createElement('div');
+const reponse1 = document.createElement('div');
+const reponse2 = document.createElement('div');
+const reponse3 = document.createElement('div');
+const reponse4 = document.createElement('div');
+const begin = document.createElement('div');
+const btnSuivant = document.createElement('div');
+reponse1.setAttribute('class' , 'reponse');
+reponse2.setAttribute('class' , 'reponse');
+reponse3.setAttribute('class' , 'reponse');
+reponse4.setAttribute('class' , 'reponse');
+
+zoneQuizz.appendChild(questionDiv);
+zoneQuizz.appendChild(reponse1);
+zoneQuizz.appendChild(reponse2);
+zoneQuizz.appendChild(reponse3);
+zoneQuizz.appendChild(reponse4);
+root.appendChild(zoneQuizz);
+root.appendChild(begin);
+root.appendChild(btnSuivant);
+
+let classBtn = 'transition duration-300 ease-in-out bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/6 mx-auto '
+let derniereQuest = false;
+
+begin.setAttribute('class' , classBtn);
+btnSuivant.setAttribute('class' , classBtn + 'hidden')
+
+begin.innerHTML = 'Jouer';
+btnSuivant.innerHTML = "suivant";
+
+const reponsesDiv = document.querySelectorAll('.reponse');
+let j = false;
+let nbQuest = 0;
+let brep = '';
+let clicked = false;
+let score = 0;
+
+
+
+
+// ___________________________________________________________________
+// utiliser json
+// ____________________________________________________________________
+
+async function loadQuestions() {
+    try {
+        const response = await fetch('./questions-cinema-series.json');
+        if (!response.ok) throw new Error('Erreur lors du chargement du fichier JSON');
+        
+        const jsonData = await response.json(); // Lit le contenu JSON
+        return extractQuestions(jsonData); // Extrait les questions
+        
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function extractQuestions(data) {
+    
+    let tableMap = data.quiz.map(
+        item => ({
+            Question : item.question,
+            bonne_reponse : item.bonne_reponse,
+            mauvaises_reponses : item.mauvaises_reponses
+        })
+    );
+    return tableMap;
+}
+
+async function displayQuestions() {
+    let table = await loadQuestions();
+    let tableQuestions = [];
+    let alea = Math.floor(Math.random()*table.length);
+    let aleaTable = [];
+    for (let i = 0 ; i < 10 ; i++){
+        while(aleaTable.includes(alea)){
+            alea = Math.floor(Math.random()*table.length)
+        }
+        aleaTable.push(alea);
+        tableQuestions.push(table[alea]);
+    }
+
+    
+    return tableQuestions
+
+}
+
+
+begin.addEventListener('click', () =>{
+    j = true;
+    if(j){
+        console.log(j);
+        displayQuestions().then(tableQuestions => {
+            brep = tableQuestions[nbQuest].bonne_reponse;
+            let tabReponse = melangeReponse(brep, tableQuestions[nbQuest].mauvaises_reponses);
+            console.log(tableQuestions[nbQuest]);
+            afficheQuestion(tableQuestions[nbQuest].Question, tabReponse);
+            begin.classList.add('hidden');
+        })
+    }   
+})
+
+btnSuivant.addEventListener('click', () =>{
+    j = true;
+    clicked = false;
+    reponse1.setAttribute('class' , 'reponse');
+    reponse2.setAttribute('class' , 'reponse');
+    reponse3.setAttribute('class' , 'reponse');
+    reponse4.setAttribute('class' , 'reponse');
+    if(derniereQuest){
+        zoneQuizz.classList.add('hidden');
+        btnSuivant.classList.add('hidden');
+        root.innerHTML = `vous avez eu ${score} bonnes réponses`;
+    } else if(j){
+        
+        displayQuestions().then(tableQuestions => {
+            brep = tableQuestions[nbQuest].bonne_reponse;
+            let tabReponse = melangeReponse(brep, tableQuestions[nbQuest].mauvaises_reponses);
+            console.log(tableQuestions[nbQuest]);
+            afficheQuestion(tableQuestions[nbQuest].Question, tabReponse);
+            begin.classList.add('hidden');
+        })
+    }   
+    if(nbQuest === 9){
+        derniereQuest = true;
+    }
+})
+
+//______________________________________________________________________
+
+
+
+function melangeReponse(brep, mrep){
+    let alea = Math.floor(Math.random()*4);
+    let reponses = [];
+    let dejachoisi = [];
+    for(let i = 0 ; i < 4 ; i++){
+        while (dejachoisi.includes(alea)){
+            alea = Math.floor(Math.random()*4);
+        }
+        dejachoisi.push(alea);
+        if(alea === 3){
+            reponses.push(brep);
+        }else{
+            
+            reponses.push(mrep[alea]);
+        }
+    }
+
+    return reponses;
+}
+
+function afficheQuestion(question, rep){
+    console.log(rep);
+    questionDiv.innerHTML = question;
+    
+    reponse1.innerHTML = rep[0];
+    reponse2.innerHTML = rep[1];
+    reponse3.innerHTML = rep[2];
+    reponse4.innerHTML = rep[3];
+}
+
+const onClick = reponsesDiv.forEach((reponse) => 
+    reponse.addEventListener('click', function() {
+        if(!clicked){
+            clicked = true;
+            
+            if(this.innerHTML === brep){
+                this.classList.add('bg-green-500');
+                btnSuivant.classList.remove('hidden');
+                if(derniereQuest){
+                    btnSuivant.innerHTML ='terminé'
+                }
+                nbQuest++;
+                j = false;
+                score++;
+                
+            }else{
+                this.classList.add('bg-red-500');
+                btnSuivant.classList.remove('hidden');
+                if(derniereQuest){
+                    btnSuivant.innerHTML ='terminé'
+                }
+                nbQuest++;
+                j = false;
+            }
+        }
+        console.log(clicked);
+    })
+);
