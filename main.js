b091e98732cabc86fbbcb7b5bdd33dee0be61439
Yannick Camarata <yannickcamarata@hotmail.com>
2024-10-28 10:12:01 +0100

    ,

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+// fonction pour le menu burger de la nav
 document.getElementById('burger').addEventListener('click', function() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('hidden');
 });
+
+// créer les éléments de la navbar
+document.createElement('header');
+document.
