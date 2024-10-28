dd88545fdfb19a875a8efe6a1ebb502cd188e526
lemon-cake <kaique-lemos@hotmail.com>
2024-10-28 15:54:49 +0100

    kaique - windtail fix

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
+const thememenu = document.getElementById("theme");
+
 // fonction pour le menu burger de la nav
 document.getElementById('burger').addEventListener('click', function() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('hidden');
+    console.log(ThemeOpen);
+    if (ThemeOpen = true){
+        thememenu.classList.toggle("hidden");
+        ThemeOpen = false;
+    }
+    if (ThemeOpen === false){
+        thememenu.classList.add("hidden");
+    }
 });
+
+// Kaique :
+
+//fonction pour le menu theme
+let ThemeOpen = false;
+
+//Mobile
+document.getElementById("theme_tel").addEventListener("click", function(){
+    const thememenu = document.getElementById("theme");
+    thememenu.classList.toggle("hidden");
+    ThemeOpen = true;
+})
+
+//Desktop
+document.getElementById("theme_desk").addEventListener("click", function(){
+    const thememenu = document.getElementById("theme");
+    thememenu.classList.toggle("md:hidden");
+    ThemeOpen = true;
+})
+
+
 
 // créer les éléments de la navbar
 document.createElement('header');
