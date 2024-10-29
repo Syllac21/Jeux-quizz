d1f9a1cf98ba0e80a23e2a850c73d1ce17a1a1f8
lemon-cake <kaique-lemos@hotmail.com>
2024-10-29 09:41:10 +0100

    re-copy sylv js

--- sylvain.js
+++ sylvain.js
@@ -1,6 +1,22 @@
+const listThemes = ['cinema-series', 'foot']
+const classThemes = 'mx-auto';
+const classThemesChoose = 'mx-auto bg-slate-600';
+const banner = document.createElement('header');
+const classBanner = 'bg-slate-900 text-white';
+banner.setAttribute('class' , classBanner);
+
+const navThemes = document.createElement('nav');
+const classNav ='flex justify-center items-center';
+navThemes.setAttribute('class' , classNav);
+
+const title = document.createElement('div');
+title.innerHTML = 'Le Quizz des semi-croustillants';
+
+let themeChosen = 'cinema';
+let chooseTheme = 'true'
 
 const root = document.getElementById('root');
-const zoneQuizz =  document.createElement('session');
+const zoneQuizz =  document.createElement('section');
 const questionDiv = document.createElement('div');
 const reponse1 = document.createElement('div');
 const reponse2 = document.createElement('div');
@@ -8,25 +24,58 @@ const reponse3 = document.createElement('div');
 const reponse4 = document.createElement('div');
 const begin = document.createElement('div');
 const btnSuivant = document.createElement('div');
+classReponseQuizz = 'reponse class bg-slate-600 text-white text-center max-w-sm rounded overflow-hidden shadow-lg mx-auto mb-5 py-3 hover:bg-sky-700';
+classQuestion = 'card-title';
+classZoneQuizz = 'card border-2 border-slate-900 bg-slate-300 w-3/6 mx-auto hidden mt-5 rounded shadow-xl'
+classTitle ='text-center text-3xl mb-5';
+
 reponse1.setAttribute('class' , 'reponse');
 reponse2.setAttribute('class' , 'reponse');
 reponse3.setAttribute('class' , 'reponse');
 reponse4.setAttribute('class' , 'reponse');
+title.setAttribute('class', classTitle);
+zoneQuizz.setAttribute('class' , classZoneQuizz);
+questionDiv.setAttribute('class' , 'reponse w-1/2 mx-auto bg-transparent mb-5');
 
 zoneQuizz.appendChild(questionDiv);
 zoneQuizz.appendChild(reponse1);
 zoneQuizz.appendChild(reponse2);
 zoneQuizz.appendChild(reponse3);
 zoneQuizz.appendChild(reponse4);
+banner.appendChild(title);
+for(let i =0 ; i < listThemes.length ; i++){
+    const newBtn = document.createElement('button');
+    newBtn.innerHTML = listThemes[i];
+    newBtn.setAttribute('class' , classThemes);
+    navThemes.appendChild(newBtn);
+}
+banner.appendChild(navThemes);
+root.appendChild(banner);
 root.appendChild(zoneQuizz);
 root.appendChild(begin);
 root.appendChild(btnSuivant);
 
-let classBtn = 'transition duration-300 ease-in-out bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/6 mx-auto '
+const btnThemes = banner.querySelectorAll('button');
+
+const onclickTheme = btnThemes.forEach((theme) => 
+    {theme.addEventListener('click',function(){
+        if(chooseTheme){
+            btnThemes.forEach((theme) => {theme.setAttribute('class' , classThemes)});
+            themeChosen = this.innerHTML;
+            this.setAttribute('class' , classThemesChoose);
+        }
+    })}
+);
+
+
+
+let classBtn = 'transition duration-300 ease-in-out bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/6 mx-auto my-7 '
 let derniereQuest = false;
 
 begin.setAttribute('class' , classBtn);
-btnSuivant.setAttribute('class' , classBtn + 'hidden')
+btnSuivant.setAttribute('class' , classBtn + 'hidden');
+
+
 
 begin.innerHTML = 'Jouer';
 btnSuivant.innerHTML = "suivant";
@@ -37,17 +86,18 @@ let nbQuest = 0;
 let brep = '';
 let clicked = false;
 let score = 0;
+let aleaTable = [];
 
-
-
-
-// ___________________________________________________________________
+// ________________________________________________________________________________________
 // utiliser json
-// ____________________________________________________________________
+// ________________________________________________________________________________________
+
+
 
 async function loadQuestions() {
+    let path = `./questions-${themeChosen}.json`
     try {
-        const response = await fetch('./questions-cinema-series.json');
+        const response = await fetch(path);
         if (!response.ok) throw new Error('Erreur lors du chargement du fichier JSON');
         
         const jsonData = await response.json(); // Lit le contenu JSON
@@ -74,30 +124,25 @@ async function displayQuestions() {
     let table = await loadQuestions();
     let tableQuestions = [];
     let alea = Math.floor(Math.random()*table.length);
-    let aleaTable = [];
-    for (let i = 0 ; i < 10 ; i++){
-        while(aleaTable.includes(alea)){
-            alea = Math.floor(Math.random()*table.length)
-        }
-        aleaTable.push(alea);
-        tableQuestions.push(table[alea]);
+    while(aleaTable.includes(alea)){
+        alea = Math.floor(Math.random()*table.length)
     }
-
-    
+    aleaTable.push(alea);
+    tableQuestions.push(table[alea]);
     return tableQuestions
-
 }
 
 
 begin.addEventListener('click', () =>{
     j = true;
+    chooseTheme = false;
     if(j){
         console.log(j);
         displayQuestions().then(tableQuestions => {
-            brep = tableQuestions[nbQuest].bonne_reponse;
-            let tabReponse = melangeReponse(brep, tableQuestions[nbQuest].mauvaises_reponses);
-            console.log(tableQuestions[nbQuest]);
-            afficheQuestion(tableQuestions[nbQuest].Question, tabReponse);
+            brep = tableQuestions[0].bonne_reponse;
+            let tabReponse = melangeReponse(brep, tableQuestions[0].mauvaises_reponses);
+            console.log(tableQuestions[0]);
+            afficheQuestion(tableQuestions[0].Question, tabReponse);
             begin.classList.add('hidden');
         })
     }   
@@ -117,10 +162,10 @@ btnSuivant.addEventListener('click', () =>{
     } else if(j){
         
         displayQuestions().then(tableQuestions => {
-            brep = tableQuestions[nbQuest].bonne_reponse;
-            let tabReponse = melangeReponse(brep, tableQuestions[nbQuest].mauvaises_reponses);
-            console.log(tableQuestions[nbQuest]);
-            afficheQuestion(tableQuestions[nbQuest].Question, tabReponse);
+            brep = tableQuestions[0].bonne_reponse;
+            let tabReponse = melangeReponse(brep, tableQuestions[0].mauvaises_reponses);
+            console.log(tableQuestions[0]);
+            afficheQuestion(tableQuestions[0].Question, tabReponse);
             begin.classList.add('hidden');
         })
     }   
@@ -129,7 +174,7 @@ btnSuivant.addEventListener('click', () =>{
     }
 })
 
-//______________________________________________________________________
+//_____________________________________________________________________________________________
 
 
 
@@ -161,6 +206,11 @@ function afficheQuestion(question, rep){
     reponse2.innerHTML = rep[1];
     reponse3.innerHTML = rep[2];
     reponse4.innerHTML = rep[3];
+    reponse1.setAttribute('class' , classReponseQuizz);
+    reponse2.setAttribute('class' , classReponseQuizz);
+    reponse3.setAttribute('class' , classReponseQuizz);
+    reponse4.setAttribute('class' , classReponseQuizz);
+    zoneQuizz.classList.remove('hidden');
 }
 
 const onClick = reponsesDiv.forEach((reponse) => 
@@ -186,8 +236,9 @@ const onClick = reponsesDiv.forEach((reponse) =>
                 }
                 nbQuest++;
                 j = false;
+                
             }
         }
-        console.log(clicked);
+        
     })
 );
